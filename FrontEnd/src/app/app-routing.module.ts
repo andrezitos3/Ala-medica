76ec89b46ec2aaec22d48f48e044781d3aa4a8ae
LeonardoOziro <144481202+LeonardoOziro@users.noreply.github.com>
2025-08-16 19:16:08 -0300

    adicionando pagina inicial mockada

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PacientesComponent } from './pages/pacientes/pacientes.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'pacientes',
+    pathMatch: 'full'
+  },
+  {
+    path: 'pacientes',
+    component: PacientesComponent
+  },
+  {
+    path: 'cadastro',
+    component: CadastroComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PacientesComponent } from './pages/pacientes/pacientes.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    PacientesComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
