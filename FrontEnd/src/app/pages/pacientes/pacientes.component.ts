e53823f264d22ff384114135e774382bfeb2c23e
LeonardoOziro <144481202+LeonardoOziro@users.noreply.github.com>
2025-08-29 22:49:36 -0300

    Adicionando chamada http mockada para a chamda de pacientes

--- FrontEnd/src/app/pages/pacientes/pacientes.component.ts
+++ FrontEnd/src/app/pages/pacientes/pacientes.component.ts
@@ -1,14 +1,5 @@
 import { Component } from '@angular/core';
-
-interface Paciente {
-  nome: string;
-  id: string;
-  internacao: boolean;
-  nascimento: string;
-  enfermidade: string;
-  alergia: string;
-  quarto: string;
-}
+import { Paciente, PacientesService } from 'src/app/services/pacientes.service';
 
 @Component({
   selector: 'app-pacientes',
@@ -19,25 +10,13 @@ interface Paciente {
 export class PacientesComponent {
   
   andar = 'Todos';
+  pacientes: Paciente[] = [];
 
-  pacientes: Paciente[] = [
-    {
-      nome: 'Eddie Chen',
-      id: '515122',
-      internacao: false,
-      nascimento: '02/07/2002',
-      enfermidade: 'Dores Intestinais',
-      alergia: 'Rivotril',
-      quarto: '01-102'
-    },
-    {
-      nome: 'Lurdes Gomes',
-      id: '00012',
-      internacao: true,
-      nascimento: '27/02/1890',
-      enfermidade: 'Osteoporose',
-      alergia: 'Monoclorreto de Sódio',
-      quarto: '07-705'
-    }
-  ];
+  constructor(private pacientesService: PacientesService) {}
+
+  ngOnInit() {
+    this.pacientesService.getAllPacientes().subscribe( data => {
+      this.pacientes = data;
+    });
+  }
 }
