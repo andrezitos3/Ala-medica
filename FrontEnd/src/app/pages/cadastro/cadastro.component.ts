0ae7289e8f29a221971e1b0a8a52100ff691a735
LeonardoOziro <144481202+LeonardoOziro@users.noreply.github.com>
2025-09-06 11:19:32 -0300

    implementação inicial do formulario da pagina de cadastro

--- FrontEnd/src/app/pages/cadastro/cadastro.component.ts
+++ FrontEnd/src/app/pages/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./cadastro.component.scss']
 })
 export class CadastroComponent {
+
+  cadastroForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.cadastroForm = this.fb.group({
+      codigo: [{ value: '00001', disabled: true }],
+      nome: ['', Validators.required],
+      nascimento: ['', Validators.required],
+      andar: ['', Validators.required],
+      internacao: [true],
+      doenca: [''],
+      alergia: ['']
+    });
+  }
+
+  salvar() {
+    if (this.cadastroForm.valid) {
+      console.log(this.cadastroForm.value);
+      alert('Paciente cadastrado com sucesso!');
+    } else {
+      alert('Preencha todos os campos obrigatórios.');
+    }
+  }
+
+  cancelar() {
+    this.cadastroForm.reset({
+      codigo: '00001',
+      internacao: true
+    });
+  }
 
 }
