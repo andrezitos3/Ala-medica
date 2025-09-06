1f3565305c542500d51017fb59ab0d1c96f8064c
Leonardo <oziro56@gmai.com>
2025-09-06 15:59:52 -0300

    adicionando funcionalidade de cadastrar no formulario de cadastro

--- FrontEnd/src/app/pages/cadastro/cadastro.component.ts
+++ FrontEnd/src/app/pages/cadastro/cadastro.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Paciente, PacientesService } from 'src/app/services/pacientes.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -10,24 +12,41 @@ export class CadastroComponent {
 
   cadastroForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(
+    private fb: FormBuilder,
+    private pacienteService: PacientesService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.cadastroForm = this.fb.group({
-      codigo: [{ value: '00001', disabled: true }],
+      id: [''],
       nome: ['', Validators.required],
       nascimento: ['', Validators.required],
       andar: ['', Validators.required],
-      internacao: [true],
-      doenca: [''],
+      quarto: [''],
+      internacao: [false],
+      enfermidade: [''],
       alergia: ['']
     });
   }
 
   salvar() {
     if (this.cadastroForm.valid) {
+      const paciente = this.cadastroForm.value as Omit<Paciente, 'id'>
       console.log(this.cadastroForm.value);
+
+      this.pacienteService.addPaciente(paciente).subscribe({
+        next: (novo) => {
+          console.log('Paciente criado:', novo);
+          this.router.navigate(['/pacientes']);
+        } ,
+        error: (err) => console.error('Erro ao salvar paciente:', err)
+      })
+
       alert('Paciente cadastrado com sucesso!');
+
+
     } else {
       alert('Preencha todos os campos obrigatórios.');
     }
@@ -35,8 +54,8 @@ export class CadastroComponent {
 
   cancelar() {
     this.cadastroForm.reset({
-      codigo: '00001',
-      internacao: true
+      codigo: '',
+      internacao: false
     });
   }
 
